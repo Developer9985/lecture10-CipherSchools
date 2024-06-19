46a02b4dafb0c386a338650e461c8656fbfa46bd
Developer9985 <94001072+Developer9985@users.noreply.github.com>
2024-06-19 16:57:34 +0530

    Add files via upload

---
+++ lecture10Loops_2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // Demonstration of pre-increment and post-incremnt operators
+
+    int i=0, j=0, k=0;
+
+    cout<<i++<<j++<<k++<<" ";
+    cout<<endl;
+    cout<<++i<<++j<<++k<<" ";
+    cout<<endl;
+
+    // taking input using for loop
+    for(int i=0; i < 5; i++){
+        int x;
+        cin>>x;
+        cout<<x<<endl;
+    }
+
+
+
+}
